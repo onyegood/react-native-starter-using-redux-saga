991d5e0dc9507813165198f480ee089497b3f61f
Ofoegbu Goodnews <goodnews@softcom.ng>
2019-07-02 15:25:15 +0100

    latest update - Setup jest and enzyme and enzyme-adapter-react-16 for unit and snapshot testing

--- src/__tests__/pages/auth/ResetPasswordScene.test.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { shallow } from "enzyme";
-import ResetPasswordScene from "../../../pages/auth/ResetPasswordScene";
-
-it("Should render component currectly", () => {
-  const wrapper = shallow(<ResetPasswordScene />);
-  expect(wrapper).toMatchSnapshot();
-});
--- src/__tests__/setupTests.js
+++
@@ -1,5 +0,0 @@
-import 'react';
-import Adapter from 'enzyme-adapter-react-16';
-import Enzyme from 'enzyme';
-
-Enzyme.configure({ adapter: new Adapter() })
